e84bc59e4f4b152935770698494837ecc1a1e3dc
YungB999 <bolureal5@gmail.com>
2024-12-29 00:48:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2347041039367"
+global.pairingNumber = "2348023846035"
 
-global.botNumber = ["2347041039367"]
+global.botNumber = ["2348023846035"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -11,18 +11,18 @@ global.loc = "1"
 global.anticall = false
 global.autoReadChat = false
 global.alwaysonline = true
-global.autoswview = false
-global.public = true
+global.autoswview = true
+global.public = false
 global.autoreact = false
 global.antidelete = false
 global.autotyping = false
-global.autoBio = true
+global.autoBio = false
 global.antiSpam = true
 global.sign = "☃️❄️"
 // ☞ ➻ ➽ ☛ 〆 
-global.bank = "Opay"
-global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.bank = "Palmpay"
+global.bankname = "༒•༺  YUNG kING  ༻•༒"
+global.accnumber = "8023846035"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
